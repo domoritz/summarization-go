3891a34da9c8f4793c2720d4c6c5f4d06ba40e0a
Dominik Moritz <domoritz@gmail.com>
2015-12-09 22:46:17 -0800

    Fix tests

--- summarize/formula_test.go
+++ summarize/formula_test.go
@@ -8,24 +8,24 @@ func TestCreate(t *testing.T) {
 
 	formula := NewFormula(cell)
 
-	if formula.tupleValue[0] != 0 {
-		t.Error("Should have value")
+	if formula.tupleCover[0] != 0 {
+		t.Error("Should have cover")
 	}
-	if formula.tupleValue[1] != 1 {
-		t.Error("Should not have value")
+	if formula.tupleCover[1] != 1 {
+		t.Error("Should not have cover")
 	}
 
 	attribute2 := Attribute{0, set, "x", nil, nil}
 	cell2 := Cell{TupleCover{1: false, 2: true}, &attribute2, "a"}
 	formula.AddCell(cell2)
 
-	if _, has := formula.tupleValue[0]; has {
-		t.Error("Should not have value")
+	if _, has := formula.tupleCover[0]; has {
+		t.Error("Should not have cover")
 	}
-	if formula.tupleValue[1] != 2 {
-		t.Error("Should have value")
+	if formula.tupleCover[1] != 2 {
+		t.Error("Should have cover")
 	}
-	if _, has := formula.tupleValue[2]; has {
+	if _, has := formula.tupleCover[2]; has {
 		t.Error("This should not be affected because we only shrink")
 	}
 }
